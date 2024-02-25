49c5a9ac418101e80a38209452541472c6547af4
Amol Khedekar <khedekaramol003@gmail.com>
2024-02-25 12:40:44 +0530

    ADD : LoginPage

--- src/app/auth/login/page.jsx
+++ src/app/auth/login/page.jsx
@@ -1,56 +1,106 @@
+"use client";
+
+import { Button, Link, TextField } from "@radix-ui/themes";
+import * as AspectRatio from "@radix-ui/react-aspect-ratio";
+import Image from "next/image";
+import React from "react";
+import { loginSchema } from "../../validation/createIssueSchema";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
 const LoginPage = () => {
+  const {
+    register,
+    control,
+    handleSubmit,
+
+    formState: { errors, isSubmitting },
+  } = useForm({
+    resolver: zodResolver(loginSchema),
+  });
+
+  const onSubmit = handleSubmit((data) => {
+    console.log(data);
+  });
+
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50">
-      <div className="max-w-md w-full p-6 space-y-8 bg-white shadow-md">
-        <div>
-          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-            Log in to your account
-          </h2>
-        </div>
-        <form className="mt-8 space-y-6">
-          
-          <div>
-            <label
-              htmlFor="email"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Email address
-            </label>
-            <input
-              id="email"
-              name="email"
-              type="email"
-              autoComplete="email"
-              required
-              className="mt-1 p-2 w-full border border-gray-300 rounded-md"
-            />
-          </div>
-          <div>
-            <label
-              htmlFor="password"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Password
-            </label>
-            <input
-              id="password"
-              name="password"
-              type="password"
-              autoComplete="current-password"
-              required
-              className="mt-1 p-2 w-full border border-gray-300 rounded-md"
-            />
-          </div>
-          <div>
-            <button
-              type="submit"
-              className="bg-blue-500 text-white p-2 rounded-md w-full"
-            >
-              Log in
-            </button>
-          </div>
-        </form>
+    <div className="flex gap-4 py-20 px-16 items-center rounded-lg bg-violet-100">
+      {/* Left Image */}
+      <div className="hidden md:block md:w-1/2 lg:w-2/3">
+        <AspectRatio.Root ratio={16 / 9}>
+          <Image
+            className=""
+            width="400"
+            height="400"
+            src="/login_img.svg"
+            alt="Login Image"
+          />
+        </AspectRatio.Root>
       </div>
+
+      {/* Right Login Form */}
+      <form
+        className="m-auto sm:w-3/4 md:w-1/2 lg:w-2/3 p-8 bg-white rounded-lg shadow-md"
+        onSubmit={onSubmit}
+      >
+        <h2 className="text-3xl font-semibold mb-4">{"Let's Get In"}</h2>
+
+        {/* Email Input */}
+        <div className="mb-4">
+          <label
+            htmlFor="email"
+            className="block text-gray-600 text-sm font-medium"
+          >
+            Email
+          </label>
+          <TextField.Root>
+            <TextField.Input
+              placeholder="john@gmail.com"
+              {...register("email")}
+            />
+          </TextField.Root>
+        </div>
+
+        {/* Password Input */}
+        <div className="mb-4">
+          <label
+            htmlFor="password"
+            className="block text-gray-600 text-sm font-medium"
+          >
+            Password
+          </label>
+          <TextField.Root>
+            <TextField.Input
+              type="password"
+              placeholder="********"
+              {...register("password")}
+            />
+          </TextField.Root>
+        </div>
+
+        {/* Remember Me Checkbox */}
+        <div className="mb-4 flex items-center">
+          <input
+            type="checkbox"
+            id="rememberMe"
+            className="mr-2"
+            {...register("rememberMe")}
+          />
+          <label htmlFor="rememberMe" className="text-sm text-gray-600">
+            Remember me
+          </label>
+        </div>
+
+        {/* Login Button */}
+        <div className="mt-8">
+          <Button className=" px-16 py-5 text-base cursor-pointer text-white rounded-md hover:bg-blue-600">
+            Login
+          </Button>
+          <p className="mt-2">
+            Not registered yet ? <Link href="#">Signup here</Link>{" "}
+          </p>
+        </div>
+      </form>
     </div>
   );
 };
--- src/app/validation/createIssueSchema.js
+++ src/app/validation/createIssueSchema.js
@@ -1,6 +1,14 @@
 import { z } from "zod";
 
+// Define Zod schema for the create Issue form
 export const createIssueSchema = z.object({
   title: z.string().min(1, "Title is required").max(255),
   description: z.string().min(1, "Description is required"),
+});
+
+// Define Zod schema for the Login form
+export const loginSchema = z.object({
+  email: z.string().email("Invalid email address").min(1, "Email is required"),
+  password: z.string().min(6, "Password must be at least 6 characters"),
+  rememberMe: z.boolean(),
 });
